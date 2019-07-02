4c918bc24ae0e3e8c8b7089d7771be2c8980f76a
estebandalelr <estebandalelr@gmail.com>
2019-07-02 12:07:41 -0500

    Add text to modelo de negocio

---
+++ components/modeloDeNegocioText.js
@@ -0,0 +1,33 @@
+export default function ModeloDeNegocioText () {
+  return (
+    <div>
+      <style jsx>{`
+        div {
+          background-image: linear-gradient(black, grey);
+          padding: 5%;
+          flex-grow: 5;
+        }
+        p {
+          text-decoration: none;
+          font-family: 'Arial';
+          color: white;
+        }
+      `}</style>
+      <p>
+        B - CONNECTED Conecta por medio de su base de datos actores económicos como: Fondeadores, Inversionistas,  Empresarios, Empresas, Emprendedores, entre otros. Para el desarrollo de negocios exitosos.
+      </p>
+      <p>
+        Compilamos mensualemnte oportunidades de negocio en un portafolio que es socializado junto con información de interés para el desarrollo de negocios entre nuestra base de datos ( si desea suscribirse puede hacerlo siguiendo el siguiente link )
+      </p>
+      <p>
+        Si por medio de nuestra conexión se realiza, celebra o fondea un negocio B- CONNECTED cobrara el fee descrito en el contrato ( Adjunto) a la empresa receptora de los fondos. En ningún momento B- CONNECTED cobra ni presta un servicio al fondeador.
+      </p>
+      <p>
+        B - CONNECTED no estructura, asesora o realiza estudios , ni interviene en las negociaciones realizadas entre las partes conectadas, estas son de responsabilidad y competencia entre las partes.
+      </p>
+      <p>
+        Para mayor información puede contactarse con nosotros al mail :  info@b-connected.co
+      </p>
+    </div>
+  )
+}
--- pages/modeloDeNegocio.js
+++ pages/modeloDeNegocio.js
@@ -1,10 +1,26 @@
 import Layout from '../components/MyLayout.js'
-import Jumbotron from '../components/Jumbotron.js'
+import ModeloDeNegocioText from '../components/modeloDeNegocioText.js'
 
 export default function ModeloDeNegocio () {
   return (
     <Layout image={'footerModeloDeNegocio.png'}>
-      <Jumbotron image='modeloDeNegocio.png' />
+      <div>
+        <style jsx>{`
+        div {
+          background-image: linear-gradient(black, grey);
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-evenly;
+        }
+        p {
+          text-decoration: none;
+          font-family: 'Arial';
+          color: white;
+        }
+      `}</style>
+        <img src='static/images/modeloDeNegocio.png' />
+        <ModeloDeNegocioText />
+      </div>
     </Layout>
   )
 }
