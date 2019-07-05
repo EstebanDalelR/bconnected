36c8e5243f2e3266e9b5fb7a7f96d05dd02d2207
estebandalelr <estebandalelr@gmail.com>
2019-07-05 00:12:29 -0500

    Add favicon and title

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,9 +1,16 @@
 import Header from './Header'
 import Footer from './Footer'
+import Head from 'next/head'
 
 export default function Layout (props) {
   return (
     <>
+      <Head>
+        <title>B - Connected </title>
+        <meta charSet='utf-8' />
+        <link rel='icon' type='image/x-icon' href='static/favicon/favicon.ico' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <style jsx>{`
         div {
           line-height: 0;
