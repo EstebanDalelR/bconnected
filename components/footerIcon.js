3ca95dc1384625a547a0d5c9f6a978f74f41a68d
estebandalelr <estebandalelr@gmail.com>
2019-09-16 22:20:12 -0500

    Add phone and address to footer

--- components/footerIcon.js
+++ components/footerIcon.js
@@ -4,11 +4,23 @@ export default function FooterIcon () {
       <style jsx>{`
         div {
           display: flex;
+          flex-direction: column;
           justify-content: center;
-          padding: 1%
+          align-items: center;
+        }
+        a, p{
+          color:white;
         }
       `}</style>
-      <img src='static/images/logoWhite.png' height='80px' />
+      <div>
+        <img src='static/images/logoWhite.png' height='80px' width="80px" />
+      </div>
+      <div>
+        <a href="https://goo.gl/maps/kzH38U2pSxSNGGuXA">
+          <p>Cra 47a #96 - 41 Of 202, Edifico Business Point, Bogotá</p>
+        </a>
+        <p>Teléfono: 7456335</p>
+      </div>
     </div>
   )
 }
